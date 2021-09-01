f7361c17fe70680cbcf2e8f9849c97221875f17d
Manuel <Huicab>
2021-09-01 13:16:18 -0500

    login

---
+++ application/controllers/LoginController.php
@@ -0,0 +1,11 @@
+<?php 
+class Login extends CI_Controller{
+
+	public function Logon()
+	{
+		$this->load->view('Login');
+		#$this->load->view('welcome_message');
+	}
+
+	
+ }
---
+++ application/views/Login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Login Audios</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<div id="cuadrologin">
+		<p>Hola este es una prueba del archivo para login</p>
+	</div>
+</body>
+</html>
